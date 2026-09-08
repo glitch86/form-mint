0544c220d54e5b168eccb054274710bfd09584b0
glitch <mdpolokahmed825@gmail.com>
2026-09-08 10:36:13 +0600

    added clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/Theme/ThemeProvider";
 import Navbar from "@/components/Shared/Navbar";
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,10 +29,28 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">
-        <ThemeProvider>
-          {/* <Navbar></Navbar> */}
-          {children}
-        </ThemeProvider>
+        <ClerkProvider>
+          {/* <header className="flex justify-end items-center gap-4 h-16">
+            <p>
+              prebuilt comp from clerk.
+            </p>
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header> */}
+          <ThemeProvider>
+            {/* <Navbar></Navbar> */}
+            {children}
+          </ThemeProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
